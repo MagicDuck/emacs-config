d2bde61c689d3a9bd1782141d8767eefaf58d07a
Stephan Badragan <sbadragan@gmail.com>
2016-06-19 21:02:06 -0700

    switched to use-package and added helm and themeing + fonts

--- init.el
+++ init.el
@@ -1,33 +1,22 @@
+;; useful to look at: http://pages.sachachua.com/.emacs.d/Sacha.html
 ;;-------------------------------------------------------------------------
 ;; package management
 
 (require 'package)
-
-;; ensures given packages are installed
-(defun ensure-package-installed (&rest packages)
-  "Assure every package is installed, ask for installation if it’s not.
-  Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
-   packages))
-
-;; make sure to have downloaded list of packages availale.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-;; package repositories
-(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
-
+                         ))
 (package-initialize)
 
+;; bootstrap - grab use-package if not available
+(unless (package-installed-p 'use-package)
+  (progn
+    (package-refresh-contents)
+    (package-install 'use-package)))
+
+(require 'use-package)
+(setq use-package-always-ensure t) ;; alway ensure packages are installed
 
 ;;-------------------------------------------------------------------------
 ;; misc customiztions
@@ -39,6 +28,7 @@
 (global-linum-mode 1) ;; display line numbers
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (fset 'yes-or-no-p 'y-or-n-p) ;; Changes all yes/no questions to y/n type
+(global-hl-line-mode 1) ;; highlight current line
 
 ;; Stop littering everywhere with save files, put them somewhere
 (setq backup-directory-alist '(("." . "~/.emacs-backups")))
@@ -53,15 +43,62 @@
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 
+;; Set locale to UTF8
+(set-language-environment 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;-------------------------------------------------------------------------
+;; theme/font
+
+;; alternatives:
+(use-package
+  ;;color-theme-sanityinc-tomorrow :init (load-theme 'sanityinc-tomorrow-day t)
+  ;;color-theme-solarized :init (load-theme 'solarized t)
+  ;;twilight-bright-theme :init (load-theme 'twilight-bright t)
+  flatui-theme :init (load-theme 'flatui t)
+  :defer t)
+
+(set-frame-font "Inconsolata 12")
+
 ;;-------------------------------------------------------------------------
 ;; helm
 
-(ensure-package-installed 'helm)
-(require 'helm-config)
+(use-package helm
+  :diminish helm-mode
+  :init
+  (progn
+    (require 'helm-config)
+    (setq helm-candidate-number-limit 100)
+    ;; From https://gist.github.com/antifuchs/9238468
+    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
+          helm-input-idle-delay 0.01  ; this actually updates things
+                                        ; reeeelatively quickly.
+          helm-yas-display-key-on-candidate t
+          helm-quick-update t
+          helm-M-x-requires-pattern nil
+          helm-ff-skip-boring-files t)
+    (helm-mode))
+  ;; TODO: look at these bindings and see which make sense
+  :bind (("C-c h" . helm-mini)
+         ("C-h a" . helm-apropos)
+         ("C-x C-b" . helm-buffers-list)
+         ("C-x b" . helm-buffers-list)
+         ("M-y" . helm-show-kill-ring)
+         ("M-x" . helm-M-x)
+         ("C-x c o" . helm-occur)
+         ("C-x c s" . helm-swoop)
+         ("C-x c y" . helm-yas-complete)
+         ("C-x c Y" . helm-yas-create-snippet-on-region)
+         ("C-x c SPC" . helm-all-mark-rings)))
+(ido-mode -1) ;; Turn off ido mode in case I enabled it accidentally as it seems to conflict with helm
 
 ;;-------------------------------------------------------------------------
 ;; evil
 
-(ensure-package-installed 'evil)
-(require 'evil)
-(evil-mode t)
+(use-package evil
+    :config
+    (evil-mode t))
